67684a639228bb58c06b800cda2d1b66b1ba916c
joe <zmc1004@gmail.com>
2024-12-18 11:29:33 +0900

    The first thymeleaf

---
+++ src/main/java/com/estsoft/blog1/controller/PageController.java
@@ -0,0 +1,25 @@
+package com.estsoft.blog1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Controller
+public class PageController {
+    @GetMapping("/thymeleaf/example")
+    public String thymeleafExample(Model model) {
+        Person person = new Person();
+        person.setId(1L);
+        person.setName("김자바");
+        person.setAge(20);
+        person.setHobbies(List.of("줄넘기", "멀리뛰기"));
+
+        model.addAttribute("person", person);
+        model.addAttribute("today", LocalDateTime.now());
+
+        return "examplePage"; //html page
+    }
+}
